aef3cdf639ac96ef1725264d0484e3221aafb30c
Alexandr <sasha_stepanov94@mail.ru>
2024-06-26 21:24:48 +0300

    Complete

--- Practice-18.5.3.cpp
+++ Practice-18.5.3.cpp
@@ -2,12 +2,12 @@
 
 void even_digits(long long number, int& result) {
   if (number % 2 == 0 && number > 0) result++;
-  if (number > 1) even_digits(number / 10, result);
+  if (number >= 1) even_digits(number / 10, result);
 }
 
 int main() {
   std::cout << "How many even digits are there in a number?\n";
-  long long num = 9223372036854775806; 
+  long long num = 1223372036854775806; 
   std::cout << "You number: " << num << " \n";
 
   int ans = 0;
