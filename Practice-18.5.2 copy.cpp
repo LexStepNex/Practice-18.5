f6744248e23da48cb7473279af2cb21ee763e1b8
Alexandr <sasha_stepanov94@mail.ru>
2024-06-26 12:50:01 +0300

    Rabbit complete

--- Practice-18.5.2 copy.cpp
+++ Practice-18.5.2 copy.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 
 int numbers_of_ways(int steps, int maxJump = 3) {
-  int temp = (steps - 1) - maxJump;
-  if (temp < 1) temp = 0;
-
-  if (steps < 1 || maxJump < 1) {
-    return 0;
-  } else if (steps == 1 || maxJump == 1) {
+  if (steps == 0) {
     return 1;
+  } else if (steps < 0 || maxJump < 1) {
+    return 0;
   } else {
-    return numbers_of_ways(steps - 1, maxJump) +
-           numbers_of_ways(steps - 1, maxJump - 1) + temp;
+    int sum = 0;
+    for (int count = maxJump; count > 0; steps--, count--) {
+      sum += numbers_of_ways(steps - 1, maxJump);
+    }
+    return sum;
   }
 }
 
 int main() {
+  std::cout << "How many options does a rabbit have"
+            <<" to jump onto the stairs when:\n";
   int n, k;
-  for (n = 0; n < 13; n++) {
-    for (k = 0; k < 13; k++) {
-      std::cout << numbers_of_ways(n, k) << "\t";
-    }
-    std::cout << "\n";
-  }
+  std::cout << "Input number of step: ";
+  std::cin >> n;
+  std::cout << "Input max jump: ";
+  std::cin >> k;
+
+  std::cout << "For max jump = " << k << ". Number of solution options: ";
+  std::cout << numbers_of_ways(n, k) << "\n";
+  
+  std::cout << "For max jump = " << 3 << ". Number of solution options: ";
+  std::cout << numbers_of_ways(n);
 }
